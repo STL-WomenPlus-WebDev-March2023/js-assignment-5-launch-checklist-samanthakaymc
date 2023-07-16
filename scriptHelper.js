d26884425c80a388df3bf8a67fe31a97e35063ef
Samantha McMurtrey <sammcmurtrey@Samanthas-MBP.attlocal.net>
2023-07-16 18:33:41 -0500

    visibility

--- scriptHelper.js
+++ scriptHelper.js
@@ -69,34 +69,34 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         alert("Cargo Level: Is Not a Number")
     }
     else {
-        faultyItems.style.visibility = "hidden";
         pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
         copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
         let launchStatus = document.getElementById("launchStatus");
+        list.style.visibility = "hidden";
         if (fuelLevel < 10000 && cargoLevel <= 10000) {
             launchStatus.style.color = "rgb(199, 37, 78)";
             fuelStatus.innerHTML = "Fuel level too low for launch";
             cargoStatus.innerHTML = "Cargo mass low enough for launch"
             launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-            list.style.visibility = "visible";
+            faultyItems.style.visibility = "visible";
         } else if (fuelLevel >= 10000 && cargoLevel > 10000) {
             launchStatus.style.color = "rgb(199, 37, 78)";
             fuelStatus.innerHTML = "Fuel level high enough for launch"
             cargoStatus.innerHTML = "Cargo mass too heavy for launch";
             launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-            list.style.visibility = "visible";
+            faultyItems.style.visibility = "visible";
         } else if (fuelLevel < 10000 && cargoLevel > 10000) {
             launchStatus.style.color = "rgb(199, 37, 78)";
             fuelStatus.innerHTML = "Fuel level too low for launch";
             cargoStatus.innerHTML = "Cargo mass too heavy for launch";
             launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-            list.style.visibility = "visible";
+            faultyItems.style.visibility = "visible";
         } else {
             launchStatus.style.color = "rgb(65, 159, 106)";
             fuelStatus.innerHTML = "Fuel level high enough for launch"
             cargoStatus.innerHTML = "Cargo mass low enough for launch"
             launchStatus.innerHTML = "Shuttle is Ready for Launch";
-            list.style.visibility = "hidden";
+            faultyItems.style.visibility = "visible";
         }
     }
 }
